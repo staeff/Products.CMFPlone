67e861244f0f7653bd8f82c1a149dcf0ee4f5c29
Eric BREHAULT <ebrehault@gmail.com>
2014-11-02 11:03:35 +0000

    declare thememapper resources + bundle

---
+++ Products/CMFPlone/static/thememapper.js
@@ -0,0 +1,16 @@
+require([
+  'jquery',
+  'mockup-registry',
+  'mockup-patterns-base',
+  'mockup-patterns-filemanager',
+  'mockup-patterns-thememapper'
+], function($, Registry, Base) {
+  'use strict';
+
+  // initialize only if we are in top frame
+  if (window.parent === window) {
+    $(document).ready(function() {
+      Registry.scan($('body'));
+    });
+  }
+});
